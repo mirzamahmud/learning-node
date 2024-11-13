3ab15c0a269408dac73b3da5b732908fe42b2596
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2024-11-14 01:36:01 +0600

    create_first_project

---
+++ first_project/app.js
@@ -0,0 +1,4 @@
+const firstProject = require('./first_project');
+
+const fetchData = firstProject.data();
+console.log(fetchData);
---
+++ first_project/first_project.js
@@ -0,0 +1,13 @@
+/// ============= create a function ========
+
+function data(){
+    return {
+        'first_name': 'Mirza',
+        'last_name': 'Mahmud',
+        'email': 'mirza.dev25@gmail.com'
+    };
+}
+
+module.exports = {
+    data: data
+}
