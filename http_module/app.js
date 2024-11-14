04a7a2b8dfa90df605b5e956834ee6b5fe468fed
mirzamahmud <mirzamahmud.cse.bubt202@gmail.com>
2024-11-14 11:13:15 +0600

    read_the_query_string

--- http_module/app.js
+++ http_module/app.js
@@ -4,7 +4,9 @@ const http = require('http');
 /// =========== to create HTTP server ===========
 http.createServer((req, res) => {
     /// ========== add http header ===========
-    res.writeHead(200, 'Success!', {'content-type': 'text/html'});
+    res.writeHead(200, 'Success!', {'content-type': 'text/plain'});
     /// ========== write a response to the client ===========
-    
+    res.write(req.url);
+    /// ========== end the response ==========
+    res.end();
 }).listen(3000);
